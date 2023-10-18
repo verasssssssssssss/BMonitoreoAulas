84008bd0d79b1f003bbfd210cbf9b4ca89948321
Sebastian Vera <sebastian.vera1901@alumnos.ubiobio.cl>
2023-10-18 07:50:26 -0300

    l

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.post("/EnviarCorreo", (req, res) => {
 //CORS middleware
 app.use(function (req, res, next) {
   //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200, https://cerulean-tarsier-37d919.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
